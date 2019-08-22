1ceccda55973dc7a55bcdeb2a347b13312e5821c
Yura Istomin <yura.ist@icloud.com>
2019-08-22 15:00:17 +0300

    [server] create an APIClient class

---
+++ Sadovod/Sources/Server/APIClient.swift
@@ -0,0 +1,15 @@
+//
+//  APIClient.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 22/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+class APIClient {
+  
+  static let shared = APIClient()
+  
+}
