78931e92a6185bc0ee5971adc1a34650f9ad4fee
Yura Istomin <yura.ist@icloud.com>
2019-08-22 13:07:23 +0300

    [main] structure project

---
+++ Sadovod/Resources/Config/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 22/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+  
+}
