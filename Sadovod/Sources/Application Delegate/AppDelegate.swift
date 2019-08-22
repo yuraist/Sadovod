3e1a06c0f38d97034ffe0cad7e0d24f0be948b15
Yura Istomin <yura.ist@icloud.com>
2019-08-22 12:57:32 +0300

    Initial Commit

---
+++ Sadovod/Sources/Application Delegate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 22/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = MainTabBarVC()
+    window?.makeKeyAndVisible()
+    
+    return true
+  }
+  
+}
+
---
+++ Sadovod/Sources/Controller/MainTabBarVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarVC.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 22/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarVC: UITabBarController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
+
