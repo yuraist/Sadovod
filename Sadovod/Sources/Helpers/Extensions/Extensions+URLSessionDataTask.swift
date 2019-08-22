a7bba8ccd5b31e6d1a5f7b74113e8610f0eea1b3
Yura Istomin <yura.ist@icloud.com>
2019-08-22 14:59:29 +0300

    [extensions] create a data task config method

---
+++ Sadovod/Sources/Helpers/Extensions/Extensions+URLSessionDataTask.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions+URLSessionDataTask.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 22/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+extension URLSessionDataTask {
+  
+  static func createDataTask(forPath path: String, queryItems items: [String: String], method: RequestMethod, completion: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+    let request = URLRequest.getRequest(forPath: path, queryItems: items, method: method)
+    let dataTask = URLSession(configuration: .default).dataTask(with: request, completionHandler: completion)
+    return dataTask
+  }
+}
