603fdc63e5de42f2cce8b61b37e5e25e72680183
Yura Istomin <yura.ist@icloud.com>
2019-08-23 20:31:24 +0300

    [models] create a token model

---
+++ Sadovod/Sources/Models/Token.swift
@@ -0,0 +1,21 @@
+//
+//  Token.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 23/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+struct Token: Codable {
+  
+  var catalogKey: String
+  var superKey: String?
+  
+  enum CodingKeys: String, CodingKey {
+    case catalogKey = "catalog_key"
+    case superKey = "super_key"
+  }
+  
+}
