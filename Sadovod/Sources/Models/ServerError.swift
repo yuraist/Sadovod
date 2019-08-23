89da7228c40c3d7c65935cea3859aec20dbc5d3e
Yura Istomin <yura.ist@icloud.com>
2019-08-23 20:31:37 +0300

    [models] create a server error model

---
+++ Sadovod/Sources/Models/ServerError.swift
@@ -0,0 +1,13 @@
+//
+//  ServerError.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 23/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+struct ServerError: Error {
+  var message: String
+}
