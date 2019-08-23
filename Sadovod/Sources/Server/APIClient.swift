757d0bcafd70adedd150f000663c14d18da2c821
Yura Istomin <yura.ist@icloud.com>
2019-08-23 20:45:57 +0300

    [api] implement check token method

--- Sadovod/Sources/Server/APIClient.swift
+++ Sadovod/Sources/Server/APIClient.swift
@@ -13,3 +13,33 @@ class APIClient {
   static let shared = APIClient()
   
 }
+
+extension APIClient {
+  
+  func checkToken(catalogKey: String, superKey: String = "", completion: @escaping ((Result<Token, ServerError>) -> Void)) {
+    let queryItems = ["catalog_key": catalogKey, "super_key": superKey]
+    
+    URLSessionDataTask.createDataTask(forPath: Endpoint.checkToken, queryItems: queryItems) { (data, _, error) in
+      if let error = error {
+        completion(.failure(ServerError(message: error.localizedDescription)))
+        return
+      }
+      
+      guard let data = data else {
+        completion(.failure(ServerError(message: "No data in response")))
+        return
+      }
+      
+      do {
+        let token = try JSONDecoder().decode(Token.self, from: data)
+        completion(.success(token))
+        return
+      } catch {
+        print(error)
+        completion(.failure(ServerError(message: error.localizedDescription)))
+        return
+      }
+    }.resume()
+  }
+  
+}
