05dca5167db28520275151bf8e779c0c1c7b22cb
Yura Istomin <yura.ist@icloud.com>
2019-08-28 12:27:32 +0300

    [models] create a TreeCategoriesResponse model

---
+++ Sadovod/Sources/Models/Response/TreeCategoriesResponse.swift
@@ -0,0 +1,13 @@
+//
+//  TreeCategoriesResponse.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 28/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+struct TreeCategoriesResponse: Codable {
+  var tree: [TreeCategory]
+}
