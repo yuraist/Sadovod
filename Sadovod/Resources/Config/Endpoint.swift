f2619d1bb2d84309d7a73520839a5e17c0bd9e52
Yura Istomin <yura.ist@icloud.com>
2019-08-28 10:09:21 +0300

    [endpoints] add endpoints for user, catalog and cart

--- Sadovod/Resources/Config/Endpoint.swift
+++ Sadovod/Resources/Config/Endpoint.swift
@@ -12,7 +12,24 @@ enum Endpoint {
   
   static let urlPrefix = "/abpro"
   
+  // MARK: - User
   static let checkToken = "/check_keys"
-  static let categoryList = "/cat_catalog"
   static let auth = "/auth"
+  static let fetchUserInfo = "/user/get"
+  
+  // MARK: - Catalog
+  static let fetchProductsOfPopularCategory = "/get_tab_broducts"
+  static let categoryList = "/cat_catalog"
+  static let fetchCategoryTree = "/get_tree_by_catalog"
+  static let fetchProductsOfCategory = "/cat_prods_catalog"
+  static let fetchProductInfo = "/product"
+  static let fetchBuyProductInfo = "/buy_product_info"
+  
+  // MARK: - Cart
+  static let addProductToCart = "/buy_product_add"
+  static let removeProductFromCart = "/buy_product_del"
+  static let removeAllItemsOfProductFromCart = "/buy_product_clear_all"
+  static let removeAllSizesOfProductFromCart = "/buy_product_clear"
+  static let fetchShortCartInfo = "/cart_total"
+  static let fetchDetailCartInfo = "/cart_info_detail"
 }
