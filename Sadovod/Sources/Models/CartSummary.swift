e69c1da4e165127422c64e5bedee631f50e6fde5
Yura Istomin <yura.ist@icloud.com>
2019-08-29 11:56:43 +0300

    [models] create a CartSummary model

---
+++ Sadovod/Sources/Models/CartSummary.swift
@@ -0,0 +1,13 @@
+//
+//  CartSummary.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 29/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+struct CartSummary: Codable {
+  var count, cost: Int
+}
