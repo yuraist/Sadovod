d2b83300490d5f716855b17ca65575147ccf679a
Yura Istomin <yura.ist@icloud.com>
2019-08-29 12:02:11 +0300

    [models] create a CartProduct model

---
+++ Sadovod/Sources/Models/Product/CartProduct.swift
@@ -0,0 +1,19 @@
+//
+//  CartProduct.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 29/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+struct CartProduct: Codable {
+  var priceId, note, name, image, count, cost, id: String
+  
+  enum CodingKeys: String, CodingKey {
+    case note, name, count, cost, id
+    case priceId = "priceId"
+    case image = "img"
+  }
+}
