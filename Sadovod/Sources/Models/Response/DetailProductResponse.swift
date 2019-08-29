8df27463db3e6de3da6bdc2afe76655bafe13154
Yura Istomin <yura.ist@icloud.com>
2019-08-29 11:10:21 +0300

    [models] create detail product response model

---
+++ Sadovod/Sources/Models/Response/DetailProductResponse.swift
@@ -0,0 +1,13 @@
+//
+//  DetailProductResponse.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 29/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+struct DetailProductResponse: Codable {
+  var product: DetailProduct
+}
