78b0eae6953334302766950794da022943623dea
Yura Istomin <yura.ist@icloud.com>
2019-08-29 11:08:34 +0300

    [models] create product option model

---
+++ Sadovod/Sources/Models/ProductOption.swift
@@ -0,0 +1,13 @@
+//
+//  ProductOption.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 29/08/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+struct ProductOption: Codable {
+  var id, name, value: String
+}
