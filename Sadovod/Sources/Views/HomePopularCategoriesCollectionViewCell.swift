0152dab335590909e38c3aa9debccd276da2edb2
Yura Istomin <yura.ist@icloud.com>
2019-09-03 17:36:05 +0300

    [views] create a home popular categories collection view cell

--- Sadovod/Sources/Views/HomePopularCategoriesCollectionViewCell.swift
+++ Sadovod/Sources/Views/HomePopularCategoriesCollectionViewCell.swift
@@ -11,10 +11,46 @@ import ConvenientLayout
 
 class HomePopularCategoriesCollectionViewCell: UICollectionViewCell {
   
+  static let cellId = "homePopularCellId"
+  
   let title: UILabel = {
     let label = UILabel()
+    label.text = "Популярные категории"
     label.font = UIFont.preferredFont(forTextStyle: .title2)
     return label
   }()
+  
+  let popularCategoriesCollectionView = PopularCategoriesCollectionView()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    addSubviews()
+    setupLayout()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  fileprivate func addSubviews() {
+    addSubviews(title, popularCategoriesCollectionView)
+  }
+  
+  fileprivate func setupLayout() {
+    title.anchor(top: topAnchor,
+                 leading: leadingAnchor,
+                 bottom: nil,
+                 trailing: trailingAnchor,
+                 padding: .init(top: 16, left: 16, bottom: 0, right: 16),
+                 size: .init(width: 0, height: 26))
+    
+    popularCategoriesCollectionView.anchor(top: title.bottomAnchor,
+                                           leading: leadingAnchor,
+                                           bottom: bottomAnchor,
+                                           trailing: trailingAnchor,
+                                           padding: .init(top: 12, left: 0, bottom: 12, right: 0),
+                                           size: .init(width: 0, height: PopularCategoryCollectionViewCell.height))
+  }
   
 }
