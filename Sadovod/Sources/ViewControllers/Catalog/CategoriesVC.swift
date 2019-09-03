579f5b12faa4a9c929c9c7a617b9dbde64b0f817
Yura Istomin <yura.ist@icloud.com>
2019-09-03 12:04:12 +0300

    [catalog] add a CategoriesVC

---
+++ Sadovod/Sources/ViewControllers/Catalog/CategoriesVC.swift
@@ -0,0 +1,27 @@
+//
+//  CategoriesVC.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 03/09/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    addSubviews()
+    setupLayout()
+  }
+  
+  fileprivate func addSubviews() {
+    
+  }
+  
+  fileprivate func setupLayout() {
+    
+  }
+}
