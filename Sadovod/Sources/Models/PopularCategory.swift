c5cdb964c05cc778ba9d5c8f87e5610d54344551
Yura Istomin <yura.ist@icloud.com>
2019-09-03 17:34:47 +0300

    [models] add a PopularCategory model

---
+++ Sadovod/Sources/Models/PopularCategory.swift
@@ -0,0 +1,16 @@
+//
+//  PopularCategory.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 03/09/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import Foundation
+
+struct PopularCategory {
+  let id: String
+  let title: String
+  let productCount: String
+  let imageName: String
+}
