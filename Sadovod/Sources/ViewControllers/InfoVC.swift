6c0b22940f98f66faf680640471a353a1317744c
Yura Istomin <yura.ist@icloud.com>
2019-09-03 12:03:18 +0300

    [main] add an InfoVC

---
+++ Sadovod/Sources/ViewControllers/InfoVC.swift
@@ -0,0 +1,27 @@
+//
+//  InfoVC.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 03/09/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import UIKit
+
+class InfoVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    addSubviews()
+    setupLayout()
+  }
+  
+  fileprivate func addSubviews() {
+    
+  }
+  
+  fileprivate func setupLayout() {
+    
+  }
+}
