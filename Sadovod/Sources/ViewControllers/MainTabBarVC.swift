c6f38b053ca3902f7bfdd7e075be68db28a2209b
Yura Istomin <yura.ist@icloud.com>
2019-09-03 12:24:35 +0300

    [main] show view controllers in the main tab bar

--- Sadovod/Sources/ViewControllers/MainTabBarVC.swift
+++ Sadovod/Sources/ViewControllers/MainTabBarVC.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class MainTabBarVC: UITabBarController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    setupViewControllers()
+  }
+  
+  fileprivate func setupViewControllers() {
+    viewControllers = [HomeVC.createNavigationController(withTitle: "Главная", imageName: "Home", tag: 0),
+                       CatalogVC.createNavigationController(withTitle: "Каталог", imageName: "Grid", tag: 1),
+                       CartVC.createNavigationController(withTitle: "Корзина", imageName: "Cart", tag: 2),
+                       ProfileVC.createNavigationController(withTitle: "Профиль", imageName: "Man", tag: 3),
+                       InfoVC.createNavigationController(withTitle: "Инфо", imageName: "Info", tag: 4)]
   }
 
 }
-
