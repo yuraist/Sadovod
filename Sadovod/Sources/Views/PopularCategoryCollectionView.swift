40e17fc6cf94ce23b24a70962c587100a682d77c
Yura Istomin <yura.ist@icloud.com>
2019-09-03 17:35:27 +0300

    [views] create a popular categories collection view

--- Sadovod/Sources/Views/PopularCategoryCollectionView.swift
+++ Sadovod/Sources/Views/PopularCategoryCollectionView.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class PopularCategoriesCollectionView: UICollectionView {
   
+  let delegateAndDataSource = PopularCategoriesDataSource()
+  
+  init() {
+    super.init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    translatesAutoresizingMaskIntoConstraints = false
+    register(PopularCategoryCollectionViewCell.self, forCellWithReuseIdentifier: PopularCategoryCollectionViewCell.cellId)
+    showsHorizontalScrollIndicator = false
+    contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+    backgroundColor = .white
+    
+    if let collectionViewLayout = collectionViewLayout as? UICollectionViewFlowLayout {
+      collectionViewLayout.scrollDirection = .horizontal
+    }
+    
+    delegate = delegateAndDataSource
+    dataSource = delegateAndDataSource
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
 }
