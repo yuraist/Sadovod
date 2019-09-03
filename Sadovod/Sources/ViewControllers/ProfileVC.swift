585b731db411c5afeb074cbfc17fd5fddc36d2fe
Yura Istomin <yura.ist@icloud.com>
2019-09-03 12:03:08 +0300

    [main] add a ProfileVC

---
+++ Sadovod/Sources/ViewControllers/ProfileVC.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileVC.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 03/09/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    addSubviews()
+    setupLayout()
+  }
+  
+  fileprivate func addSubviews() {
+    
+  }
+  
+  fileprivate func setupLayout() {
+    
+  }
+}
