10d4ded3fb15bfa216a7f2fe9e9d525b3e5fbc6c
Yura Istomin <yura.ist@icloud.com>
2019-09-03 12:02:45 +0300

    [main] add a CatalogVC

---
+++ Sadovod/Sources/ViewControllers/CatalogVC.swift
@@ -0,0 +1,27 @@
+//
+//  CatalogVC.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 03/09/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import UIKit
+
+class CatalogVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    addSubviews()
+    setupLayout()
+  }
+  
+  fileprivate func addSubviews() {
+    
+  }
+  
+  fileprivate func setupLayout() {
+    
+  }
+}
