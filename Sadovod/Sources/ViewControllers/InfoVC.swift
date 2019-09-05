ddadf77e409ab6a3d36dcdaec1554d3080aad1df
Yura Istomin <yura.ist@icloud.com>
2019-09-05 17:45:12 +0300

    [info] create an info table view

--- Sadovod/Sources/ViewControllers/InfoVC.swift
+++ Sadovod/Sources/ViewControllers/InfoVC.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class InfoVC: UIViewController {
+class InfoVC: UITableViewController {
   
   static let navigationTitle = "Инфо"
+  fileprivate let cellId = "infoCellId"
+  fileprivate let titles = ["Частые вопросы", "Задать вопрос", "Доставка", "Оплата"]
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -18,6 +20,7 @@ class InfoVC: UIViewController {
     setupNavigationBar()
     addSubviews()
     setupLayout()
+    setupTableView()
   }
   
   fileprivate func setupNavigationBar() {
@@ -30,5 +33,22 @@ class InfoVC: UIViewController {
   
   fileprivate func setupLayout() {
     
+  }
+  
+  fileprivate func setupTableView() {
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return titles.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+    
+    cell.accessoryType = .disclosureIndicator
+    cell.textLabel?.text = titles[indexPath.row]
+    
+    return cell
   }
 }
