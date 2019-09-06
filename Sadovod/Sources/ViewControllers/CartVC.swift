e4a509ea460d80e77fca475c2e1ce353048989e9
Yura Istomin <yura.ist@icloud.com>
2019-09-06 19:32:20 +0300

    [cart] present the cart collection view

--- Sadovod/Sources/ViewControllers/CartVC.swift
+++ Sadovod/Sources/ViewControllers/CartVC.swift
@@ -12,12 +12,18 @@ class CartVC: UIViewController {
   
   static let navigationTitle = "Корзина"
   
+  fileprivate let collectionViewDataSource = CartCollectionViewDataSource()
+  fileprivate let collectionView = CartCollectionView()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     setupNavigationBar()
     addSubviews()
     setupLayout()
+    
+    setupCollectionViewDataSource()
+    setupCollectionViewDelegate()
   }
   
   fileprivate func setupNavigationBar() {
@@ -25,10 +31,30 @@ class CartVC: UIViewController {
   }
   
   fileprivate func addSubviews() {
-    
+    view.addSubview(collectionView)
   }
   
   fileprivate func setupLayout() {
-    
+    view.backgroundColor = .white
+    collectionView.anchor(top: view.topAnchor,
+                          leading: view.safeAreaLayoutGuide.leadingAnchor,
+                          bottom: view.bottomAnchor,
+                          trailing: view.safeAreaLayoutGuide.trailingAnchor)
   }
+  
+  fileprivate func setupCollectionViewDataSource() {
+    collectionView.dataSource = collectionViewDataSource
+  }
+  
+  fileprivate func setupCollectionViewDelegate() {
+    collectionView.delegate = self
+  }
+}
+
+extension CartVC: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: view.frame.width, height: CartCollectionViewCell.cellHeight + 28)
+  }
+  
 }
