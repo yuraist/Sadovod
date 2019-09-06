cce28b608eea6376194ccf0faf4219f9aa40c90c
Yura Istomin <yura.ist@icloud.com>
2019-09-06 19:00:30 +0300

    [config] add static colors constants

--- Sadovod/Resources/Config/Constants.swift
+++ Sadovod/Resources/Config/Constants.swift
@@ -6,12 +6,22 @@
 //  Copyright © 2019 Yura Istomin. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct Constants {
   
   static let appName = "ios_sadovod"
   static let baseUrl = "tk-sad.ru"
   static let scheme = "https"
+  
+  struct Color {
+    static let blue = UIColor(named: "Blue")
+    static let paleBlue = UIColor(named: "PaleBlue")
+    static let grayButton = UIColor(named: "ButtonGray")
+    static let graySubtitle = UIColor(named: "SubtitleGray")
+    static let tagBlue = UIColor(named: "TagBlue")
+    static let tagGreen = UIColor(named: "TagGreen")
+    static let tagYellow = UIColor(named: "TagYellow")
+  }
   
 }
