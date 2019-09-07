f54e8d578231af2c2e1ecc432126824d225f5503
Yura Istomin <yura.ist@icloud.com>
2019-09-07 13:32:21 +0300

    [cart] add a test cart product with an image

--- Sadovod/Sources/DataSources/CartCollectionViewDataSource.swift
+++ Sadovod/Sources/DataSources/CartCollectionViewDataSource.swift
@@ -10,14 +10,31 @@ import UIKit
 
 class CartCollectionViewDataSource: NSObject, UICollectionViewDataSource {
   
-  var products = [CartProduct(priceId: "2", note: "No note", name: "Платье", image: "...", count: "5", cost: "727 РУБ.", id: "1")]
+  var products = [
+    CartProduct(priceId: "2",
+                note: "No note",
+                name: "Платье",
+                image: "http://tk-sad.ru/ipk/g3/0/1/1/1/0/462446.jpg",
+                count: "5",
+                cost: "727 РУБ.",
+                id: "1")
+  ]
   
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return products.count
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    return collectionView.dequeueReusableCell(withReuseIdentifier: CartCollectionViewCell.cellId, for: indexPath)
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CartCollectionViewCell.cellId, for: indexPath) as! CartCollectionViewCell
+    
+    let product = products[indexPath.item]
+    
+    cell.title.text = product.name
+    cell.image.loadImage(withUrlString: product.image)
+    cell.priceLabel.text = product.cost
+    cell.sizeLabel.text = product.note
+    
+    return cell
   }
   
 }
