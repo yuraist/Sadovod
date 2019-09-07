7f7d8eb09e9855a8657129f8470d02883023fd63
Yura Istomin <yura.ist@icloud.com>
2019-09-07 14:00:38 +0300

    [views] create a new  file for the small gray button class

---
+++ Sadovod/Sources/Views/Common/SmallGrayButton.swift
@@ -0,0 +1,39 @@
+//
+//  SmallGrayButton.swift
+//  Sadovod
+//
+//  Created by Yura Istomin on 07/09/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+import UIKit
+
+class SmallGrayButton: UIButton {
+  
+  init(title: String) {
+    super.init(frame: .zero)
+    
+    setTitle(title.uppercased(), for: .normal)
+    setupAppearance()
+  }
+  
+  init(icon: UIImage) {
+    super.init(frame: .zero)
+    
+    setImage(icon.withRenderingMode(.alwaysTemplate), for: .normal)
+    setupAppearance()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  fileprivate func setupAppearance() {
+    tintColor = Constants.Color.graySubtitle
+    backgroundColor = Constants.Color.grayButton
+    setTitleColor(Constants.Color.graySubtitle, for: .normal)
+    titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+    layer.cornerRadius = 10
+  }
+  
+}
