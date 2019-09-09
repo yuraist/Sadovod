051d003f2eaf403902a3be1dbfcf158aa37e414d
Yura Istomin <yura.ist@icloud.com>
2019-09-09 12:01:52 +0300

    [home] present unpaid orders if a user is authorized

--- Sadovod/Sources/DataSources/HomeCollectionViewDataSource.swift
+++ Sadovod/Sources/DataSources/HomeCollectionViewDataSource.swift
@@ -15,8 +15,15 @@ class HomeCollectionViewDataSource: NSObject, UICollectionViewDataSource {
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
     if indexPath.item == 0 {
       return collectionView.dequeueReusableCell(withReuseIdentifier: HomePopularCategoriesCollectionViewCell.cellId, for: indexPath) as! HomePopularCategoriesCollectionViewCell
+    } else if indexPath.item == 1 {
+      if (Token.shared.userIsAuthorized ?? false) {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: UnpaidOrdersCollectionViewCell.cellId, for: indexPath)
+      } else {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: AuthorizeCollectionViewCell.cellId, for: indexPath)
+      }
     }
     
     return collectionView.dequeueReusableCell(withReuseIdentifier: AuthorizeCollectionViewCell.cellId, for: indexPath)
