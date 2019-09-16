b8a84955dbf4ab3dc82a41d5c85a3deb2349b048
Yura Istomin <yura.ist@icloud.com>
2019-09-16 13:44:36 +0300

    [main] add RxSwift

---
+++ Pods/Target Support Files/Pods-Sadovod/Pods-Sadovod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Sadovod : NSObject
+@end
+@implementation PodsDummy_Pods_Sadovod
+@end
---
+++ Pods/Target Support Files/Pods-SadovodTests/Pods-SadovodTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SadovodTests : NSObject
+@end
+@implementation PodsDummy_Pods_SadovodTests
+@end
